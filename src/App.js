7133eb0be6c6c04c837bbd2139180e2ac07bb174
Nadim-Nahle <nahlenadim@gmail.com>
2023-11-04 19:38:46 +0200

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser, EditGuesser, Create, SimpleForm, TextInput } from 'react-admin';
 import './App.css';
-import { dataProvider } from './dataProvider';
+import dataProvider from './dataProvider';
+import ProductCreate from './ProductCreate';
+import ProductList from './ProductList';
 
 function App() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="products" list={ListGuesser} />
+      <Resource name="products" list={ProductList} edit={EditGuesser} create={ProductCreate}/>
       <Resource name="users" list={ListGuesser} />
     </Admin>
   );
 }
+
 
 export default App;
