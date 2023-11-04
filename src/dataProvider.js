6c9920a3857bca8b2734048c0bc18d62a9c46c94
Nadim-Nahle <nahlenadim@gmail.com>
2023-11-04 19:39:02 +0200

    Update data provider

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,6 +1,36 @@
-// in src/dataProvider.ts
+import { fetchUtils } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
-export const dataProvider = jsonServerProvider(
-    "http://127.0.0.1:5001/ecommerce-nadim/us-central1/api"
+const httpClient = (url, options = {}) => {
+  if (!options.headers) {
+    options.headers = new Headers({ Accept: 'application/json' });
+  }
+  options.headers.set('auth-api', '5eb80460-69ac-4df7-af84-021db8043a59');
+  return fetchUtils.fetchJson(url, options);
+};
+
+const jsonServerDataProvider = jsonServerProvider(
+  'http://127.0.0.1:5001/ecommerce-nadim/us-central1/api',
+  httpClient
 );
+
+const customDataProvider = {
+  ...jsonServerDataProvider,
+
+  // Override the create method to handle errors
+  create: async (resource, params) => {
+    try {
+      const response = await jsonServerDataProvider.create(resource, params);
+      return response;
+    } catch (error) {
+      if (error.status === 400) {
+        // Handle validation errors or other specific error codes
+        const message = error.message;
+        throw new Error(message);
+      }
+      throw error;
+    }
+  },
+};
+
+export default customDataProvider;
