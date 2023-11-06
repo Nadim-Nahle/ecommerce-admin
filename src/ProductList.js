e32982bb07ba826c1df0280b42c29bd5a8490acf
Nadim-Nahle <nahlenadim@gmail.com>
2023-11-06 18:05:23 +0200

    add search to product

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { Datagrid, DeleteButton, EditButton, List, TextField } from 'react-admin'
+import { Datagrid, DeleteButton, EditButton, List, TextField, TextInput } from 'react-admin'
 
 const ProductList = (props) => {
+
+  const postFilters = [
+    <TextInput label="Search" source="q" alwaysOn />,
+];
+
   return (
-    <List {...props}>
+    <List {...props} filters={postFilters}>
         <Datagrid>
         <TextField source='id'/>
         <TextField source='ref'/>
