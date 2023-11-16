e11ebb0403b0aa10e0a30c3893c5038b2a100765
Nadim-Nahle <nahlenadim@gmail.com>
2023-11-16 20:25:05 +0200

    import order

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import SignIn from './SignIn';
 import ReactAdmin from './ReactAdmin';
 import RequireAuth from './RequireAuth';
+import Orders from './Order';
 
 const App = () => {
   return (
     
       <Routes>
         <Route path="/login" element={<SignIn />} />
-        
+        <Route path="/orders" element={<Orders />} />
         {/* Define other routes using the <Route> component */}
         <Route element={<RequireAuth  />}>
         <Route path="/*" element={<ReactAdmin />} />
